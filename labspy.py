e07244b2fdc57afca7e360d507b284ae0a5e0159
alyashafira <56963083+alyashafira@users.noreply.github.com>
2019-10-29 12:53:34 +0700

    Add files via upload
    code programs

---
+++ labspy.py
@@ -0,0 +1,18 @@
+print   (" >>> Program menentukan bilangan terbesar dari 3 buah bilangan <<<")
+
+
+o = int (input(" masukan bilangan Ke Satu: "))
+p = int (input(" masukan bilangan Ke dua: "))
+q = int (input(" masukan bilangan Ke tiga: "))
+
+if o > p and o > q :
+    print (o, " = adalah bilangan terbesar dari 3 buah bilangan yang diinput")
+elif p > o and p > q :
+    print (p, " = adalah bilangan terbesar dari 3 buah bilangan yang diinpu")
+else :
+    print (q, " = adalah bilangan terbesar dari 3 buah bilangan yang diinput")
+
+
+
+print (">>>>>>>>>>>>>>>>>>>>>>>> ========= <<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+
